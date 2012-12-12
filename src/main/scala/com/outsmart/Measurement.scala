e2352a31497178db2f3082d39c8117a95340fd77
Vadim <vadimbobrov@gmail.com>
2012-12-12 16:43:38 -0500

    added implicit conversion from Long

--- src/main/scala/com/outsmart/Measurement.scala
+++ src/main/scala/com/outsmart/Measurement.scala
@@ -6,3 +6,7 @@ package com.outsmart
 class Measurement(val value : Long, val timestamp : Long) {
   override def toString = "ts: " + timestamp + " value: " + value
 }
+
+object Measurement {
+  implicit def longToMeasurement(x: Long) = new Measurement(x, System.currentTimeMillis)
+}
