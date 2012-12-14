257abcfc7038a9015bcfc9b0607ed84959861419
Vadim <vadimbobrov@gmail.com>
2012-12-14 13:36:51 -0500

    HBase related comments

--- src/main/scala/com/outsmart/dao/ScannerImpl.scala
+++ src/main/scala/com/outsmart/dao/ScannerImpl.scala
@@ -10,6 +10,23 @@ import com.outsmart.{Settings, Measurement}
 */
 class ScannerImpl extends Scanner {
 
+  /**
+    Sometimes it might be necessary to find a specific row, or the one just before the re-
+    quested row, when retrieving data. The following call can help you find a row using
+    these semantics:
+
+      <code>Result getRowOrBefore(byte[] row, byte[] family) throws IOException</code>
+
+    You need to specify the row you are looking for, and a column family. The latter is
+    required because, in HBase, which is a column-oriented database, there is no row if
+    there are no columns.
+
+    ...all columns of the specified column family were returned, including their latest
+    values. You could use this call to     quickly retrieve all the latest values from
+    an entire column family—in other words, all columns contained in the given column
+    family—based on a specific sorting pattern
+   */
+
   def scan(customer : String, location : String, wireid : String, start : Long, end : Long) : Array[Measurement] = {
 
     val table = TableFactory.getTable()
@@ -33,7 +50,8 @@ class ScannerImpl extends Scanner {
 
     iterator foreach (res => {
       val value = res.getValue(Bytes.toBytes(Settings.ColumnFamilyName), Bytes.toBytes(Settings.QualifierName))
-      output = new Measurement(Bytes.toLong(value), RowKeyUtils.getTimestamp(res.getRow)) :: output
+      val row = res.getRow
+      output = new Measurement(Bytes.toLong(value), RowKeyUtils.getTimestamp(row)) :: output
     })
 
     results.close()
--- src/main/scala/com/outsmart/dao/TableFactory.scala
+++ src/main/scala/com/outsmart/dao/TableFactory.scala
@@ -19,6 +19,22 @@ object TableFactory {
   config.set("hbase.zookeeper.quorum", Settings.Host)
 
 
+  /** If you were ever required to access the write buffer content, you would find that
+    ArrayList<Put> getWriteBuffer() can be used to get the in- ternal list of buffered
+    Put instances you have added so far calling
+    table.put(put).
+      I mentioned earlier that it is exactly that list that makes HTable not safe
+    for multithreaded use. Be very careful with what you do to that list when
+    accessing it directly. You are bypassing the heap size checks, or you
+    might modify it while a flush is in progress!
+
+    Since the client buffer is a simple list retained in the local process mem-
+    ory, you need to be careful not to run into a problem that terminates
+    the process mid-flight. If that were to happen, any data that has not yet
+    been flushed will be lost! The servers will have never received that data,
+    and therefore there will be no copy of it that can be used to recover from
+    this situation.
+  */
   def getTable() : HTable = {
     val table = new HTable(config, Settings.TableName)
     table.setAutoFlush(false)
