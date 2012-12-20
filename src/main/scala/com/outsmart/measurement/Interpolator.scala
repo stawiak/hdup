6b4f9e32187706297860be9fbc4f0db054bb80e0
Vadim <vadimbobrov@gmail.com>
2012-12-20 17:04:43 -0500

    interpolation - done, not tested

--- src/main/scala/com/outsmart/measurement/Interpolator.scala
+++ src/main/scala/com/outsmart/measurement/Interpolator.scala
@@ -44,15 +44,36 @@ object Interpolator {
       // if there is a minute boundary between current and next measurement
       // calculate interpolated value for it and set next minute boundary
       if (sorted(i).timestamp > currentMinBoundary) {
+
         // take 4 points and find intersection
+        val intersect = findIntersection(
+          sorted(i - 2).timestamp, sorted(i - 2).value,
+          sorted(i - 1).timestamp, sorted(i - 1).value,
+          sorted(i).timestamp, sorted(i).value,
+          sorted(i + 1).timestamp, sorted(i + 1).value
+        )
 
-        //if (intersection found)
-        //else use linear interpolation
+        var computedValue: Double = 0
 
-        // compare current minute boundary with intersection
-        // compute interpolation using ether lower or upper 2 points
+        if (intersect._1 > 0) {
+          // intersection found
+          // compare current minute boundary with intersection
+          // compute interpolation using ether lower or upper 2 points
+          if (intersect._1 == currentMinBoundary)
+            computedValue = intersect._2
+          else if (intersect._1 < currentMinBoundary)
+            computedValue = linearInterpolate(currentMinBoundary, intersect._1, intersect._2, sorted(i).timestamp, sorted(i).value)
+          else
+            computedValue = linearInterpolate(currentMinBoundary, sorted(i - 1).timestamp, sorted(i - 1).value, intersect._1, intersect._2)
+
+        } else {
+          // no intersection - either parallel or outside period
+          //use linear interpolation between two measurements
+          computedValue = linearInterpolate(currentMinBoundary, sorted(i - 1).timestamp, sorted(i - 1).value, sorted(i).timestamp, sorted(i).value)
+        }
+
         // add to output
-        output = new TimedValue(currentMinBoundary, ) :: output
+        output = new TimedValue(currentMinBoundary, computedValue) :: output
 
         // set next minute boundary
         currentMinBoundary += 60000
@@ -69,9 +90,11 @@ object Interpolator {
    * the output is rounded to long again
    *
    * params: xy12 - 2 points of the first line, xy34 - 2 points of the second line
-   * @return 0 if parallel, -1 if intersect outside [x2, x3], X coordinate of intersection otherwise
+   *
+   * prereq: strictly in ascending order by X
+   * @return (0,0) if parallel, (-1,-1) if intersect outside [x2, x3], coordinates of intersection otherwise
    */
-  def intersection(x1: Double, y1: Double, x2: Double, y2: Double, x3: Double, y3: Double, x4: Double, y4: Double): Long = {
+  def findIntersection(x1: Double, y1: Double, x2: Double, y2: Double, x3: Double, y3: Double, x4: Double, y4: Double): (Long, Double) = {
     // ensure strictly ascending
     assert(x1 < x2 && x2 < x3 && x3 < x4)
 
@@ -80,11 +103,21 @@ object Interpolator {
 
     // if parallel
     if (slope1 == slope2)
-      return 0
+      return (0, 0)
 
-    val res = math.round((slope1 * x1 - y1 - slope2 * x3 + y3) / (slope1 - slope2))
+    val x = math.round((slope1 * x1 - y1 - slope2 * x3 + y3) / (slope1 - slope2))
+    val y = (slope1 * slope2 * (x1 - x3) + slope1 * y3 - slope2 * y1) / (slope1 - slope2)
 
-    if (res >= x2 && res <= x3) res else -1
+    if (x >= x2 && x <= x3) (x, y) else (-1, -1)
   }
 
+  /**
+   * Linear interpolation at point x on a line with x1y1, x2y2
+   * @param x interpolation point converted to double for math
+   * @return interpolation value
+   */
+  def linearInterpolate(x: Double, x1: Double, y1: Double, x2: Double, y2: Double): Double = {
+    assert(x1 != x2)
+    (x - x1) * (y2 - y1)/(x2 - x1) + y1
+  }
 }
