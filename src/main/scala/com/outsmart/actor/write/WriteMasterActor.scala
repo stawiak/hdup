bdb4e573561f57009ba218b304810d1da5b284f8
Vadim <vadimbobrov@gmail.com>
2013-01-02 15:59:34 -0500

    supervisor strategy added

--- src/main/scala/com/outsmart/actor/write/WriteMasterActor.scala
+++ src/main/scala/com/outsmart/actor/write/WriteMasterActor.scala
@@ -5,66 +5,75 @@ import com.outsmart.measurement.Measurement
 import com.outsmart.Settings
 import com.outsmart.dao.Writer
 import akka.routing.{FromConfig, RoundRobinRouter}
+import akka.actor.SupervisorStrategy.{Stop, Resume, Restart, Escalate}
 
 
 /**
  * @author Vadim Bobrov
  */
 case object Flush
-case object Stop
+case object StopWriter
 case object WorkDone
 case class WriteWork(measurements: Seq[Measurement])
 
 class WriteMasterActor(writerCreator : () => Writer) extends Actor {
 
-  import context._
-  // Since a restart does not clear out the mailbox, it often is best to terminate the children upon failure and re-create them explicitly from the supervisor
+	import context._
+	// Since a restart does not clear out the mailbox, it often is best to terminate the children upon failure and re-create them explicitly from the supervisor
 
-  var measurements = List[Measurement]()
-  val workerRouter = actorOf(Props(new WriterActor(writerCreator())).withRouter(FromConfig()), name = "workerRouter")
-  var numberOfBatches = 0
-  var numberOfDone = 0
-  var receivedAll = false
-  var counter = 0
+	var measurements = List[Measurement]()
+	val workerRouter = actorOf(Props(new WriterActor(writerCreator())).withRouter(FromConfig()), name = "workerRouter")
+	var numberOfBatches = 0
+	var numberOfDone = 0
+	var receivedAll = false
+	var counter = 0
 
-  protected def receive: Receive = {
+	override val supervisorStrategy =
+		OneForOneStrategy(maxNrOfRetries = 3) {
+			//case _: NullPointerException      	⇒ Resume
+			case _: Exception     				⇒ Restart
+			case _: IllegalArgumentException 	⇒ Stop
+			case _: Throwable                	⇒ Escalate
+		}
 
-    case msmt : Measurement => {
+	protected def receive: Receive = {
 
-      counter += 1
-      measurements = msmt :: measurements
+		case msmt : Measurement => {
 
-      if(measurements.length == Settings.BatchSize) {
-        workerRouter ! WriteWork(measurements)
-        numberOfBatches += 1
-        measurements = List[Measurement]()
-      }
+			counter += 1
+			measurements = msmt :: measurements
 
-    }
+			if(measurements.length == Settings.BatchSize) {
+				workerRouter ! WriteWork(measurements)
+				numberOfBatches += 1
+				measurements = List[Measurement]()
+			}
 
-    case WorkDone => {
-      numberOfDone += 1
-      println("number of done " + numberOfDone + " out of " + numberOfBatches)
-      if (receivedAll && numberOfDone == numberOfBatches)
-        parent ! WorkDone
-    }
+		}
 
-    case Flush =>  {
-      println("flush received at " + counter)
-      println("remaining msmts " + measurements.length)
-      workerRouter ! WriteWork(measurements)
-      numberOfBatches += 1
-      measurements = List[Measurement]()
-      receivedAll = true
-    }
+		case WorkDone => {
+			numberOfDone += 1
+			println("number of done " + numberOfDone + " out of " + numberOfBatches)
+			if (receivedAll && numberOfDone == numberOfBatches)
+				parent ! WorkDone
+		}
+
+		case Flush =>  {
+			println("flush received at " + counter)
+			println("remaining msmts " + measurements.length)
+			workerRouter ! WriteWork(measurements)
+			numberOfBatches += 1
+			measurements = List[Measurement]()
+			receivedAll = true
+		}
 
 
-    case Stop => {
-      println("received stop")
-      // Stops this actor and all its supervised children
-      stop(self)
-    }
+		case StopWriter => {
+			println("received stop")
+			// Stops this actor and all its supervised children
+			stop(self)
+		}
 
-  }
+	}
 
 }
