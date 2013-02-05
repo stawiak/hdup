83364a66329e98bc5c9ebf86e414ff9d00cfb7c2
Vadim Bobrov <vadimbobrov@gmail.com>
2013-02-04 21:18:16 -0500

    equals fixed

--- src/main/scala/com/os/measurement/Measurement.scala
+++ src/main/scala/com/os/measurement/Measurement.scala
@@ -20,15 +20,17 @@ class Measurement(
 
 	override def toString = "measurement " + customer + "/" + location + "/" + wireid + " " + super.toString
 
-	override def equals(that : Any)  = {
-		that.isInstanceOf[Measurement] &&
-		that.asInstanceOf[Measurement].customer == this.customer &&
-		that.asInstanceOf[Measurement].location == this.location &&
-		that.asInstanceOf[Measurement].wireid == this.wireid &&
-		that.asInstanceOf[Measurement].timestamp == this.timestamp &&
-		that.asInstanceOf[Measurement].energy == this.energy &&
-		that.asInstanceOf[Measurement].current == this.current &&
-		that.asInstanceOf[Measurement].vampire == this.vampire
+	override def equals(other : Any)  = other match {
+    case that: Measurement =>
+      that.customer == this.customer &&
+      that.location == this.location &&
+      that.wireid == this.wireid &&
+      that.timestamp == this.timestamp &&
+      that.energy == this.energy &&
+      that.current == this.current &&
+      that.vampire == this.vampire
+    case _ => false
 	}
 
+  //TODO: hashCode!!!
 }
