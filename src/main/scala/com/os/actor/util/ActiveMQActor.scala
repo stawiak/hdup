d12197634473fe0e8df016a5602548732520ad1e
Vadim Bobrov <vadimbobrov@gmail.com>
2013-02-15 19:39:10 -0500

    moved connection to receive. Do not override in subclasses!

--- src/main/scala/com/os/actor/util/ActiveMQActor.scala
+++ src/main/scala/com/os/actor/util/ActiveMQActor.scala
@@ -3,10 +3,12 @@ package com.os.actor.util
 import akka.actor.{ActorLogging, Actor}
 import org.apache.activemq.ActiveMQConnectionFactory
 import javax.jms._
+import concurrent.duration.Duration
 
 /**
  * @author Vadim Bobrov
  */
+case class Connect()
 abstract class ActiveMQActor(host: String, port: Int, queue: String) extends Actor with ActorLogging with MessageListener {
 
 	var connection: Connection = _
@@ -17,18 +19,23 @@ abstract class ActiveMQActor(host: String, port: Int, queue: String) extends Act
 		self ! msg
 	}
 
+	override def receive: Receive = {
+		// connect in receive because supervisor won't restart after failure in preStart
+		case Connect =>
+			val connectionFactory = new ActiveMQConnectionFactory("tcp://" + host + ":" + port)
+			connection = connectionFactory.createConnection()
+			//connection.setExceptionListener(this);
+
+			session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE)
+			val destination = session.createQueue(queue)
+			consumer = session.createConsumer(destination)
+
+			consumer.setMessageListener(this)
+			connection.start()
+	}
 
 	override def preStart() {
-		val connectionFactory = new ActiveMQConnectionFactory("tcp://" + host + ":" + port)
-		connection = connectionFactory.createConnection()
-		//connection.setExceptionListener(this);
-
-		session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE)
-		val destination = session.createQueue(queue)
-		consumer = session.createConsumer(destination)
-
-		consumer.setMessageListener(this)
-		connection.start()
+		context.system.scheduler.scheduleOnce(Duration.Zero, self, Connect)
 	}
 
 	override def postStop() {
