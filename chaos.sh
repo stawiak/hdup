ae1bca03a48528921e020b10ac7857798274543d
Vadim <vadimbobrov@gmail.com>
2013-02-27 17:54:39 -0500

    moved jar and dependencies to bin directory

--- chaos.sh
+++ chaos.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -Xms1G -Xmx8G -Dcom.sun.management.jmxremote.port=9999 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -jar chaos.jar
+java -Xmx100M -Dcom.sun.management.jmxremote.port=9999 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -jar bin/chaos.jar
